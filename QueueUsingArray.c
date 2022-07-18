f33c16f251cde21a8b36be4935e0cab66a7c2637
Steezy <104679786+steezygod@users.noreply.github.com>
2022-07-18 15:55:37 +0530

    Update QueueUsingArray.c

--- QueueUsingArray.c
+++ QueueUsingArray.c
@@ -10,7 +10,7 @@ int q[SIZE], front=-1, rear=-1, i , item, op, temp;
 
 void main(){
 	while(1){
-		printf("1.Insertion\n 2.Deletion \n 3.Display\n");
+		printf("1.Insertion\n 2.Deletion \n 3.Display\n 4.Exit\n");
 		printf("Choose any option: ");
 		scanf("%d", &op);
 		switch(op){
