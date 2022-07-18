61eb73c34bcd40582601f219753fc3a11795bdd3
Steezy <104679786+steezygod@users.noreply.github.com>
2022-07-18 12:55:55 +0530

    Update StackUsingArray.c

--- StackUsingArray.c
+++ StackUsingArray.c
@@ -1,70 +1,83 @@
-#include<stdio.h>  
-#include<stdlib.h>  
-#define size 5
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 5
 
-void  push();
-void  pop();
-void peek();
+void push(int);
+void pop();
 void display();
+int isfull();
+int isempty();
 
-int top = -1, stack[size], i;
+int stack[SIZE], top=-1, item, op, temp, i;
+
 void main(){
-
-	int ch;
 	while(1){
-		printf("\n1.Push\n 2.Pop\n 3.Peek\n 4.Display\n 5.Quit\n");
-		printf("Enter your choice : ");
-		scanf("%d",&ch);
-		switch(ch){
-			case 1 :push();
-				break;
-			case 2 :pop();
-				break;
-			case 3:	peek();
-				break;
-			case 4: display();
-				break;
-			case 5:	exit(0);
-			default:printf("Wrong choice\n");
-		}//switch
-	}//while
-}//main
+		printf("1.Insertion\n 2.Deletion \n 3.Display \n 4.IsFull \n 5.IsEmpty \n");
+		printf("Choose any option: ");
+		scanf("%d", &op);
+		switch(op){
+			case 1: push(item);
+			break;
+			case 2: pop();
+			break;
+			case 3: display();
+			break;
+			case 4: isfull();
+			break;
+			case 5: isempty();
+			break;
+			case 6: exit(0);
+			default: printf("Wrong Choice\n");
+		}
+	}
+}
 
-void push(){
-	if(top == size-1)
-		printf("Stack Overflow\n");
+void push(int item){
+	if(top >= SIZE){
+		printf("\nStack Overflow");
+	}
 	else{
+		printf("\nEnter an element to push: ");
+		scanf("%d", &item);
 		top++;
-		int ele;
-		printf("Enter element you want to push: ");
-		scanf("%d", &ele);
-		stack[top] = ele;
+		stack[top] = item;
 	}
 }
 
 void pop(){
-	if(top == -1)
-		printf("Stack Underflow\n");
+    if(top == -1)
+	printf("\nStack Underflow");
 	else{
-		printf("deleted item from stack is %d\n", stack[top]);
+		temp = stack[top];
+		printf("The deleted item is %d\n", stack[top]);
 		top--;
-	}
-}
-
-void peek(){
-	if(top == -1)
-		printf("Stack Underflow\n");
-	else{
-		printf("%d\n", stack[top]);
 	}
 }
 
 void display(){
 	if(top == -1)
-		printf("Stack Underflow\n");
+	printf("\nStack Underflow");
 	else{
 		printf("Elements in stack are: ");
 		for(i=0; i<=top; i++)
 			printf("%4d", stack[i]);
+	}
+	printf("\n");
+}
+
+int isfull(){
+	if(top >= SIZE){
+		printf("\nStack Overflow");
+	}
+	else{
+		return 1;
+	}
+}
+
+int isempty(){
+	if(top == -1)
+	printf("\nStack Underflow");
+	else{
+		return 1;
 	}
 }
