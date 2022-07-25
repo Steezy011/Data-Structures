4d51838f277344a0521a6da56f6b31aa3e8e1909
Steezy <104679786+steezyneo@users.noreply.github.com>
2022-07-25 17:23:16 +0530

    Update cqusingll.c

--- cqusingll.c
+++ cqusingll.c
@@ -1,74 +1,78 @@
 #include<stdio.h> 
 #include<stdlib.h>
-int choice,i,item; struct node {
-int data; 
-struct node *link;
-}*front,*rear,*new,*ptr1,*ptr;
-int enqueue(); 
-int dequeue(); 
-int display();
+
+struct node {
+	int data; 
+	struct node *link;
+}*front, *rear, *new, *ptr1, *ptr;
+
+void enq(); 
+void dq(); 
+void display();
+
+int ch,i,item; 
+
 void main() {
 	front=rear=NULL;
-	printf("\n select menu\n");
 	while(1)
 		{
-		printf("\n1.Enqueue \n2.Dequeue \n3.Display \n4.Exit");
-		printf("\nEnter ur choice: ");
-		scanf("%d",&choice);
-		switch(choice)
+		printf("\n1.Enqueue \n2.Dequeue \n3.Display \n4.Exit\n");
+		printf("Enter ur choice: ");
+		scanf("%d",&ch);
+		switch(ch)
 		{
-		case 1: enqueue();
+		case 1: enq();
 		break;
-		case 2: dequeue();
+		case 2: dq();
 		break;
 		case 3: display();
 		break;
 		case 4: exit(0);
-		default: printf("\nWrong choice.");
-		}/*end of switch*/
-	}/*end of while*/
-}/*end of main*/
-
-int enqueue()
-{
-	new=malloc(sizeof(struct node));
-	printf("\nEnter the item: ");
-	scanf("%d",&item);
-	new->data=item;
-	if(front==NULL)
-		front=new;
-	else
-		rear->link=new;
-		rear=new;
-		rear->link=front;
-		return;
-}/*end of enqueue()*/
-
-int dequeue()
-{
-	if(front==NULL) 
-	printf("\nThe circular list is empty.");
-	else if(front==rear) {
-		printf("\nThe deleted element is: %d",front->data); 
-		front=rear=NULL;
+		default: printf("Wrong choice.\n");
+		}
 	}
-	else {
-		printf("\nThe deleted element is: %d",front->data); 
-		front=front->link; 
-		rear->link=front;
-	} return; 
-}/*end of dequeue*/
+}
 
-int display() {
-	ptr=front; 
-	if(front==NULL) 
-		printf("\nThe circular list is empty.");
-		else {
-		printf("\nElements in the list are: "); 
-		do {
-			printf(" %d",ptr->data); 
-			ptr=ptr->link;
-		} while(ptr!=front); 
-		return;
-	}/*end of else*/
-}/*end of display*/
+void enq(){
+	new = malloc(sizeof(struct node));
+	printf("Enter the element you want to insert: \n");
+	scanf("%d", &item);
+	new->data = item;
+	
+	if(front == NULL && rear == NULL){
+		front = rear = new;
+	}
+	else{
+		rear->link = new;
+		new->link = front;
+		rear = rear->link;
+	}
+}
+
+void dq(){
+	if(front == NULL && rear == NULL)
+	printf("Queue is empty\n");
+	else if(front == rear){
+		printf("The deleted item from queue is %d\n", front->data);
+	}
+	else{
+		printf("The deleted item from queue is %d\n", front->data);
+		ptr = front;
+		front = front->link;
+		rear->link = front;
+		free(ptr);
+	}
+}
+
+void display(){
+	ptr = front;
+	if(front == NULL && rear == NULL)
+	printf("Queue is empty\n");
+	else{
+		while(ptr->link!= front){
+			printf("%4d", ptr->data);
+			ptr = ptr->link;
+		}
+		printf("%4d", ptr->data);
+	}
+}
